226bd7142b77dc723247ca9e5f3b1395730bb68d
rebooted943 <116455857+rebooted943@users.noreply.github.com>
2023-06-12 13:43:15 +0200

    creata struttura componenti ed aggiunto un db mock

--- esercizioTrinaware/src/app/app.module.ts
+++ esercizioTrinaware/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistrationComponent } from './auth/registration/registration.component';
+import { PasswordRecoveryComponent } from './auth/password-recovery/password-recovery.component';
+import { PopupComponent } from './shared/popup/popup.component';
+import { StoreListComponent } from './store/store-list/store-list.component';
+import { StoreDetailComponent } from './store/store-detail/store-detail.component';
+import { WarehouseComponent } from './store/warehouse/warehouse.component';
+import { ProductFormComponent } from './store/product-form/product-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    PasswordRecoveryComponent,
+    PopupComponent,
+    StoreListComponent,
+    StoreDetailComponent,
+    WarehouseComponent,
+    ProductFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ esercizioTrinaware/src/app/store/store-detail/store-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-store-detail',
+  templateUrl: './store-detail.component.html',
+  styleUrls: ['./store-detail.component.scss']
+})
+export class StoreDetailComponent {
+
+}
